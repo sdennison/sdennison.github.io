b3c36f1486c3ce6f7d72bfbb4ac8cbea4382fb4a
sdennison <sdennison@kippnyccp.org>
2016-06-07 12:09:12 -0400

    Update Js.js

--- Js.js
+++ Js.js
@@ -1,6 +1,11 @@
 
-    <script type="text/javascript" src="Js.js"></script>
-    <script>
-function myFunction() {
-    var x = document.createElement("BUTTON");
-   $("#chicken")click(function)
+    //<script type="text/javascript" src="Js.js"></script>
+    //<script>
+//function myFunction() {
+    //var x = document.createElement("BUTTON");
+   //$("#chicken")click(function)
+$(document).ready(function(){
+    $("p").mouseenter(function(){
+        $(this).css("color", "orange");
+    })
+})
