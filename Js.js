f0667319c69bb5dd345e3c57658d83f38af906c8
sdennison <sdennison@kippnyccp.org>
2016-06-09 12:23:03 -0400

    Update Js.js

--- Js.js
+++ Js.js
@@ -1,5 +1,9 @@
 $( document ).ready(function(){
         $("p").mouseenter(function(){
             $(this).css("color", "orange");
-    });
+        });
+        $("button").click(function(){
+            $("#shamar").css("position", "fixed").css("top","0").css("right","0");
+        });
+        
 });
